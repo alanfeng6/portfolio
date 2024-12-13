6c4b8977555d357676b660d51b2735d81a02eb09
alanfeng6 <fengalan@msu.edu>
2024-12-12 21:31:46 -0500

    test3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey this is Alan
+          Hey this is Alan.
         </p>
         <a
           className="App-link"
